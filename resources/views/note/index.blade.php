ffc029fe4072f53c598f936b84e97b0a31060012
Ifeoluwa <omolekeifeoluwa2005@gmail.com>
2024-11-13 11:57:13 +0100

    New Changes added

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -7,34 +7,24 @@
         <div class="card">
             <div class="card-body">
                 <div class="row">
-                    <div class="col-md-3"></div>
-                    <div class="col-md-6">
-                        <table class="table ">
-                            <tr>
-                                <th>Note</th>
-                                <th></th>
-                            </tr>
-                            <tbody>
-                                @foreach ($notes as $note)
-                                <tr>
-                                    <td>{{ Str::words($note->note, 30)}}</td>
-                                    <td>
-                                        <form action="{{ route('note.destroy',$note ) }}" method="post">
-                                            <a href="{{ route('note.show',$note) }}" class="btn btn-primary">Views</a>
-                                            <a href="{{ route('note.edit',$note) }}" class="btn btn-secondary">Edit</a>
-                                            @csrf
-                                            @method('DELETE')
-                                            <button class="btn btn-danger" type="submit">Delete</button>
-                                        </form>
-                                    </div>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="col-md-3"></div>
+                    @foreach ($notes as $note)
+                    <div class="col-md-4 mb-3">
+                        <div class="card mb-3">
+                            <div class="card-body bg-warning">
+                                {{ Str::words($note->note, 30)}}
 
+                                <form action="{{ route('note.destroy',$note ) }}" method="post">
+                                    <a href="{{ route('note.show',$note) }}" class="btn btn-primary">Views</a>
+                                    <a href="{{ route('note.edit',$note) }}" class="btn btn-secondary">Edit</a>
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger" type="submit">Delete</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                    @endforeach
                 </div>
 
                 {{-- <div class="note-buttons">
